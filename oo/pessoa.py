d5879d9a7db71546360a3f92f0bee0cf59888720
millenamartines <millenamartines@gmail.com>
2020-08-12 13:16:41 -0300

    Criado e removido atributo dinâmico de objeto do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -12,7 +12,7 @@ class Pessoa:
 
 if __name__ == '__main__':
     julio = Pessoa(nome ='Julio')
-    luciano = Pessoa(julio, nome ='Guilherme')
+    luciano = Pessoa(julio, nome ='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -20,4 +20,10 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Ramalho'
+    #print(luciano.sobrenome)
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(julio.__dict__)
+
 
